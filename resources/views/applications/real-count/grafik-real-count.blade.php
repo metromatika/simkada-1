3b498cc779178160ee206f6bdc84bf7d6025afd5
ALBERT LUKAS TALUPAN PANGARIBUAN <albertlukastalupan1@gmail.com>
2023-09-11 14:18:27 +0700

    add pie chart untuk realcount

--- resources/views/applications/real-count/grafik-real-count.blade.php
+++ resources/views/applications/real-count/grafik-real-count.blade.php
@@ -1,5 +1,144 @@
 @extends('layouts.user_type.auth')
 
 @section('content')
-ini halaman /real-count/grafik-real-count.blade.php
+    <div class="row mt-4">
+        <div class="col-lg-6 mb-lg-0 mb-4">
+            <div class="card">
+                <div class="card-body px-0 pt-0 pb-2" id="Kecamatan">
+                    <div class="table-responsive p-0">
+                        <table class="table align-items-center mb-0">
+                            <thead>
+                                <tr>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        No
+                                    </th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Kelurahan
+                                    </th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        L (orang)
+                                    </th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        P (orang)
+                                    </th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Total
+                                    </th>
+                                    <th
+                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                        Action
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="text-center">
+                                        <p class="text-xs font-weight-bold mb-0">1.</p>
+                                    </td>
+                                    <td class="text-center">
+                                        <p class="text-xs font-weight-bold mb-0">Lalang</p>
+                                    </td>
+                                    <td class="text-center">
+                                        <p class="text-xs font-weight-bold mb-0">13</p>
+                                    </td>
+                                    <td class="text-center">
+                                        <p class="text-xs font-weight-bold mb-0">40</p>
+                                    </td>
+                                    <td class="text-center">
+                                        <p class="text-xs font-weight-bold mb-0">53</p>
+                                    </td>
+                                    <td class="text-center">
+                                        <a href="#" class="mx-3" data-bs-toggle="tooltip"
+                                            data-bs-original-title="Edit">
+                                            <i class="fas fa-edit text-secondary"></i>
+                                        </a>
+                                        <span>
+                                            <i class="cursor-pointer fas fa-trash text-secondary"></i>
+                                        </span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 mb-lg-0 mb-4">
+            <div class="card z-index-2">
+                <div class="card-body p-3">
+                    <div class="bg-gradient-dark border-radius-lg py-3 pe-1 mb-3">
+                        <div class="chart">
+                            <canvas id="pie-chart" class="chart-canvas" height="300"></canvas>
+                        </div>
+                    </div>
+                    <h6 class="ms-2 mt-4 mb-0"> Hitung Cepat Kab/Kota Aceh Selatan </h6>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
+@push('grafik-real-count')
+    <script>
+        var ctx4 = document.getElementById("pie-chart").getContext("2d");
+
+        new Chart(ctx4, {
+            type: "pie",
+            data: {
+                labels: ['Joko Widodo', 'Maruf Amin', 'Prabowo'],
+                datasets: [{
+                    label: "Projects",
+                    weight: 9,
+                    cutout: 0,
+                    tension: 0.9,
+                    pointRadius: 2,
+                    borderWidth: 2,
+                    backgroundColor: ['#ea0606', '#82d616', '#fbcf33'],
+                    data: [35.31, 31.21, 33.48],
+                    fill: false
+                }],
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        display: true,
+                    }
+                },
+                interaction: {
+                    intersect: false,
+                    mode: 'index',
+                },
+                scales: {
+                    y: {
+                        grid: {
+                            drawBorder: false,
+                            display: false,
+                            drawOnChartArea: false,
+                            drawTicks: false,
+                        },
+                        ticks: {
+                            display: false
+                        }
+                    },
+                    x: {
+                        grid: {
+                            drawBorder: false,
+                            display: false,
+                            drawOnChartArea: false,
+                            drawTicks: false,
+                        },
+                        ticks: {
+                            display: false,
+                        }
+                    },
+                },
+            },
+        });
+    </script>
+@endpush

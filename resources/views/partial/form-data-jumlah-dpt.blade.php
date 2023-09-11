118f1c78b252be74aeff1bfe0deb2293998f56c4
ALBERT LUKAS TALUPAN PANGARIBUAN <albertlukastalupan1@gmail.com>
2023-09-11 15:36:37 +0700

    modal form kiri dan tabel kanan

--- resources/views/partial/form-data-jumlah-dpt.blade.php
+++ resources/views/partial/form-data-jumlah-dpt.blade.php
@@ -1,34 +1,87 @@
 <div class="modal fade" id="formDataTPS" tabindex="-1" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal" role="document">
+    <div class="modal-dialog modal-lg modal-dialog-scrollable modal-dialog-centered modal" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="modalTitleId">Form Master TPS</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <div class="row justify-content-start align-items-start g-2">
-                    <div class="col">
-                        <label for="" class="form-label">Provinsi</label>
-                        <select class="form-select" aria-label="Default select example">
-                            <option selected>Pilih Provinsi</option>
-                            <option value="1">One</option>
-                          </select>
-                    </div>
-                    <div class="col">
-                        <label for="" class="form-label">Kabupaten/Kota</label>
-                        <select class="form-select" aria-label="Default select example">
-                            <option selected>Pilih Kabupaten/Kota</option>
-                            <option value="1">One</option>
-                          </select>
-                    </div>
-                </div>
-                <div class="row justify-content-start align-items-start g-2">
-                    <div class="col">
-                        <label for="" class="form-label">Kecamatan</label>
-                        <select class="form-select" aria-label="Default select example">
-                            <option selected>Pilih Kecamatan</option>
-                            <option value="1">One</option>
-                          </select>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h5 class="text-sm">DATA WILAYAH</h5>
+                            <div class="col">
+                                <label class="form-label"> Provinsi </label>
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>Pilih Provinsi</option>
+                                    <option value="1">One</option>
+                                </select>
+                            </div>
+                            <div class="col">
+                                <label class="form-label"> Kabupaten/Kota </label>
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>Pilih Kabupaten/Kota</option>
+                                    <option value="1">One</option>
+                                </select>
+                            </div>
+                            <div class="col">
+                                <label class="form-label"> Kecamatan </label>
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>Pilih Kecamatan</option>
+                                    <option value="1">One</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <h5 class="text-sm">JUMLAH DPT PER KELURAHAN / DESA</h5>
+                            <div class="table-responsive p-0">
+                                <table class="table align-items-center mb-0">
+                                    <thead>
+                                        <tr>
+                                            <th
+                                                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                                No.
+                                            </th>
+                                            <th
+                                                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                                KELURAHAN / DESA
+                                            </th>
+                                            <th
+                                                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                                L
+                                            </th>
+                                            <th
+                                                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                                P
+                                            </th>
+                                            <th
+                                                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                                Total
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td class="text-center">
+                                                <p class="text-xs font-weight-bold mb-0">1.</p>
+                                            </td>
+                                            <td class="text-center">
+                                                <p class="text-xs font-weight-bold mb-0">KEUDE BAKONGAN</p>
+                                            </td>
+                                            <td class="text-center">
+                                                <p class="text-xs font-weight-bold mb-0">3</p>
+                                            </td>
+                                            <td class="text-center">
+                                                <p class="text-xs font-weight-bold mb-0">40</p>
+                                            </td>
+                                            <td class="text-center">
+                                                <p class="text-xs font-weight-bold mb-0">43</p>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>

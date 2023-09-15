2a2158605cc58e45f4930a2db647c4b8792b9284
ALBERT LUKAS TALUPAN PANGARIBUAN <albertlukastalupan1@gmail.com>
2023-09-15 15:40:55 +0700

    Form Isu modal

--- resources/views/applications/monitoring-isu-politik/daftar-isu.blade.php
+++ resources/views/applications/monitoring-isu-politik/daftar-isu.blade.php
@@ -16,8 +16,9 @@
                         </div>
                     </div>
                     <div class="d-flex align-items-center justify-content-center">
-                        <a href="#" class="btn bg-gradient-primary btn-sm mb-0 text-lg" type="button">+&nbsp; Daftar Isu</a>
+                        <a href="#" class="btn bg-gradient-primary btn-sm mb-0 text-lg" data-bs-toggle="modal" data-bs-target="#formDaftarIsu" type="button">+&nbsp; ISU</a>
                     </div>
+                    @include('applications.monitoring-isu-politik.partial.form-daftar-isu')
                 </div>
             </div>
             <div class="card-body px-0 pt-0 pb-2">
---
+++ resources/views/applications/monitoring-isu-politik/partial/form-daftar-isu.blade.php
@@ -0,0 +1,97 @@
+<div class="modal fade" id="formDaftarIsu" tabindex="-1" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+    <div class="modal-dialog modal-xl modal-dialog-scrollable modal-dialog-centered modal" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalTitleId">Form Tim</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-3">
+                        <div class="col">
+                            <label for="formFile" class="form-label">Unggah Bukti</label>
+                            <input class="form-control" type="file" id="formFile">
+                        </div>
+                    </div>
+                    <div class="col-5">
+                        <div class="col">
+                            <label for="" class="form-label">Jenis Isu</label>
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Isu</option>
+                                <option value="1">Lapangan</option>
+                                <option value="2">Media Massa</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <label class="form-label"> Dampak Isu </label>
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected> Dampak Isu </option>
+                                <option value="1">Positif</option>
+                                <option value="2">Negatif</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <label for="" class="form-label"> Tanggal Isu </label>
+                            <input type="date" class="form-control" name="" id="" placeholder="">
+                        </div>
+                        <div class="col">
+                            <label for="" class="form-label"> Keterangan </label>
+                            <input type="text" class="form-control" name="" id="" placeholder="">
+                        </div>
+                        <div class="col">
+                            <label for="" class="form-label"> Nama Pelapor </label>
+                            <input type="text" class="form-control" name="" id="" placeholder="">
+                        </div>
+                        <div class="form-check mt-2">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-label" for="flexCheckDefault">
+                                <a class="form-label">
+                                    Isu Wilayah <br>
+                                    <i class="fw-lighter">
+                                        Silahkan tandai jika Isu pada wilayah spesifik
+                                    </i>
+                                </a>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="col">
+                            <label for="" class="form-label">Wilayah Isu</label>
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Pilih Tingkat</option>
+                                <option value="1">Kabupaten/Kota</option>
+                                <option value="2">Kecamatan</option>
+                                <option value="3">Kelurahan</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <label class="form-label"> Provinsi </label>
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Pilih Provinsi</option>
+                                <option value="1">One</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <label class="form-label"> Kabupaten/Kota </label>
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Pilih Kabupaten/Kota</option>
+                                <option value="1">One</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <label class="form-label"> Kecamatan </label>
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Pilih Kecamatan</option>
+                                <option value="1">One</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                <button type="button" class="btn btn-primary">Simpan</button>
+            </div>
+        </div>
+    </div>
+</div>
